125d322bab279085db94ad030f5cb4257e7cdddc
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2023-12-16 20:18:55 +0200

    Empty React app.

---
+++ tic_tac_toe_app/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [x_player, set_x_player] = useState('');
+  const [y_player, set_y_player] = useState('');
+
+  return (
+    <>
+      <h1>Tic Tac Toe </h1>
+    </>
+  )
+}
+
+export default App
